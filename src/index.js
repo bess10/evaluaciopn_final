6925dacc3d3caf349d0ed1d0098c95915f1bc8fe
Bessy <salgadobessy10@gmail.com>
2022-11-15 17:15:05 +0100

    añadiendo fichero css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 //const router = require('./routes/routes.js');
 const {join} = require('node:path');
+//aqui arrancaremos el servidor
 const cors = require('cors');
-
 const app = express();
 const PUERTO = 4000;
 
 app.use(cors());
+//directorio para ficheros estaticos
 app.use(express.static(join(__dirname, 'public')));
+
 app.use(require('./routes/routes.js'))
+//asignamos el puerto
 app.listen(PUERTO, console.log('Server running...'));
